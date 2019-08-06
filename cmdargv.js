85e7761ff951e2f83633c7e428bd8e94b8195240
AlexLeoTW <alex@alexleo.click>
2019-08-06 18:05:09 +0800

    add redirect

--- cmdargv.js
+++ cmdargv.js
@@ -3,12 +3,22 @@ const argv = require('yargs')
     'Usage: $0 [--port <port>]')
   .option('domain', {
       description: 'domain or ip',
-      demandOption: true
+      default: 'localhost',
   })
   .option('port', {
       description: 'which port to listen on',
       default: 3000,
       alias: 'p'
+  })
+  .option('image', {
+      description: 'which image to use(og:image)',
+      default: 'logo.png',
+      alias: 'i',
+  })
+  .option('redirect', {
+      description: 'urls to direct to',
+      default: 'https://github.com/AlexLeoTW/ogp_redirect',
+      alias: 'r',
   })
   .help()
   .argv
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index', {url: `http://${cmdargv.domain}:${cmdargv.port}`});
+  res.render('index', cmdargv);
 });
 
 app.use(function(err, req, res, next){
